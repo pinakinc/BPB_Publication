7440fe03241b49e0825bd619c3cc42886e18e044
Pinakin Chaubal <chaubalpinakin@gmail.com>
2020-01-05 12:19:01 +0530

    Defaults

---
+++ keywordframework/src/test/java/keywordframework/Default_Values.java
@@ -0,0 +1,11 @@
+package keywordframework;
+
+public class Default_Values {
+    public static final String BROWSER = "firefox";
+    public static final String PLATFORM = "WIN8";
+    public static final String ENVIRONMENT = "bstack";
+    public static String DEFT_BROWSER = null;
+    public static String DEFT_PLATFORM = null;
+    public static String DEFT_ENVIRONMENT = null;
+
+}
